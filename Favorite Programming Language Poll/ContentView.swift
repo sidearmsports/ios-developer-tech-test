de949d3d37e33881282cd9e7810f7955d9fea507
Jeffrey Gray <jeffreygray@sidearmsports.com>
2022-04-21 11:03:14 -0400

    Initial Commit

---
+++ Favorite Programming Language Poll/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Favorite Programming Language Poll
+//
+//  Created by Jeffrey Gray on 4/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Favorite Programming Language Poll/Favorite_Programming_Language_PollApp.swift
@@ -0,0 +1,17 @@
+//
+//  Favorite_Programming_Language_PollApp.swift
+//  Favorite Programming Language Poll
+//
+//  Created by Jeffrey Gray on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Favorite_Programming_Language_PollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
